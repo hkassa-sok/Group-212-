afc94de0b39ecdf27a974f4eb7b7a41dde399577
hkassa-sok <hkassa@terpmail.umd.edu>
2026-04-14 21:21:13 -0400

    Heyaw - added actual find_sets code

---
+++ hand_reader.py
@@ -0,0 +1,25 @@
+def find_sets(hand):
+    """
+    Finds all the common sets that a player has in their hand in order to
+    highlight how many points they possibly have
+    """
+    groups_sets = {}
+    # Ranking the cards based on what group they are in
+    for card in hand:
+        ranks = card[0]
+
+        if ranks not in groups_sets:
+            groups_sets[ranks] = []
+
+        groups_sets[ranks].append(card)
+
+        # Find valid sets
+        sets = []
+        for ranks in groups_sets:
+           if len(groups_sets[ranks]) >= 3:
+               sets.append(groups_sets[ranks])
+               
+        return sets
+              
+
+
